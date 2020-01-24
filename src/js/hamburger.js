61898c794557894a55f2c191ef34e831b074772e
Nikita <collidem31@gmail.com>
2020-01-24 22:52:56 +0200

    header completed

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,7 +1,9 @@
 const wrapper = document.querySelector('.header__menu')
 const nav = document.querySelector('.header__nav')
+const body = document.querySelector('body')
 wrapper.onclick = function() {
     const menu = document.querySelector('.menu__icon')
     menu.classList.toggle('menu__icon_active')
     nav.classList.toggle('header__nav_active')
+    body.classList.toggle('lock')
 }
